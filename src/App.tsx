4a7e6639711118f63ad3f80cc5902b59a96adf6b
Naffster89 <naffsmith@hotmail.co.uk>
2026-03-20 16:30:37 +0900

    fix: add SPA 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ScrollToTop from './components/ScrollToTop';
+import NotFoundPage from './pages/NotFoundPage';
 
 // Lazy load components
 const HomePage = lazy(() => import('./pages/HomePage'));
@@ -59,6 +60,7 @@ function App() {
           <Route path="/java/*" element={<JavaPage />} />
           <Route path="/csharp/*" element={<CSharpPage />} />
           <Route path="/go/*" element={<GoPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
     </Router>
